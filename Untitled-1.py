1f08a3e03121fdcc09b5a5c4f10173c1b7ec6342
İremHazal <iremhazal28@gmail.com>
2026-05-12 14:56:25 +0300

    ilk commit

---
+++ Untitled-1.py
@@ -0,0 +1,19 @@
+website = "https://www.sadikturan.com"
+course = "Python Kursu : Baştan Sona Python Programlama Rehberiniz (40 saat)"
+
+# 1 - 'course' karakter dizisinde kaç karakter bulunmaktadır? 
+
+result = len(course)
+
+# 2 - 'website' içinden www karakterlerini alın 
+print(website[8:11])
+
+# 3 - 'website' içinden com karakterlerini alın 
+print(website[23:])
+# 4 - 'course' içinden ilk 15 ve son 15 karakterlerini alın 
+print(course[:15])
+print(course[15:])
+# 5 - 'course' ifadesindeki karakterleri tersten yazdırın. 
+
+
+print(result)
